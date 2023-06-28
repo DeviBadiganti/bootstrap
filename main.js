b69d0c6f4014c821b5a6cef4a8b91d746a1c2a5e
SailakshmiMogali <sailakshmi930@gmail.com>
2023-06-28 16:30:06 +0530

    28/6/2023

--- main.js
+++ main.js
@@ -155,16 +155,16 @@
   
   // // js events
  
-  // const icon =document.querySelector(".icon");
-  // const password=document.querySelector(".password");
-  // icon.addEventListener("click",function passwordClick(){
-  //   icon.classList.toggle("fa-eye");
-  //   icon.classList.toggle("fa-eye-slash");
-  //   console.log("hhhhf");
-  //   password.type=password.type==="password"?"text":"password"
-  // })
+  const icon =document.querySelector(".icon");
+  const password=document.querySelector(".password");
+  icon.addEventListener("click",function passwordClick(){
+    icon.classList.toggle("fa-eye");
+    icon.classList.toggle("fa-eye-slash");
+    console.log("hhhhf");
+    password.type=password.type==="password"?"text":"password"
+  })
   
-  // // form hide
+  // form hide
    const formHide=document.querySelector(".form-hide");
    function myFunction(){
    return formHide.classList.toggle("form-hide-active");
@@ -173,44 +173,372 @@
 
   //  array methods 
    
-  // push method
-  const array=[ "web","development","app","mobile","ios","digital","marketing"];
-  const array1=array.push("software");
-   console.log(array);
-//   //  pop method
+//   // push method
+//   const array=[ "web","development","app","mobile","ios","digital","marketing"];
+//   const array1=array.push("software");
+//    console.log(array);
+// //   //  pop method
 //   const array2=array.pop();
 //    console.log(array);
 //   //  shift method
 //   const array3=array.shift();
 //    console.log(array);
-//   //  unshift method
-//  const array4=array.unShift("software");
-//     console.log(array);
-//     // splice method
+// //   //  unshift method
+// //  const array4=array.unShift("software");
+// //     console.log(array);
+// //     // splice method
 //     const array5=array.splice( 2,1,"developer","window");
 //     console.log(array);
-//     // slice method
-   const array6=array.slice(2,5);
-   console.log(array6);
-   const array7=array.slice(2);
-   console.log(array7);
-    // tostring method
-    const array8=array.toString();
-    console.log(array8);
-    // join method
-    const array9=array.join("*");
-    console.log(array9);
-  //  max and min method
-  const sai=[4,65,28,75,95,23,13,109,356,893,4784];
-    console.log(Math.max.apply (null,sai));
-    console.log(Math.min.apply(null,sai));
+// //     // slice method
+//    const array6=array.slice(2,5);
+//    console.log(array6);
+//    const array7=array.slice(2);
+//    console.log(array7);
+//     // tostring method
+//     const array8=array.toString();
+//     console.log(array8);
+//     // join method
+//     const array9=array.join("*");
+//     console.log(array9);
+//   //  max and min method
+//   const sai=[4,65,28,75,95,23,13,109,356,893,4784];
+//     console.log(Math.max.apply (null,sai));
+//     console.log(Math.min.apply(null,sai));
+
+//     // methods
+
+//     const data=[{id:1,name:"Sai",age:21,group:"bsc"},
+//                 {id:2,name:"Devi",age:2,group:"bsc"},
+//                 {id:3,name:"Bharathi",age:30  ,group:"bsc"}];
+
+
+//     // map method
+//         const devi=data.map(function myFunction(items){
+//           return items.age*2;
+//         });       
+//   console.log(devi);
+
+// // //   // filter method
+//       const sai= data.filter(function myFunction(items){
+//           return items.age<20;
+//         });       
+//     console.log(sai);
+
+// // find method
+
+//  const findData= data.find(function myFunction(items){
+//           return items.age>20;
+//         });       
+//     console.log(findData);
+
+
+// //     // reduce method
+//    const reduceData=data.reduce(function myFunction(currentValue,items){
+//     return items.age+currentValue
+//    },0);
+//    console.log(reduceData);
    
-  // // cards
-  // let cards="";
-  // objArray.map(function myFunction(item)){
-  //   cards+='<div>
-  //   <h1>$(item.name)</h1>
-  //   <h4>$(item.age)</h4>
-  //   <h>$(item.des)</h1>
-  // }
-  // const ronaldo=[{id:1,title:Image1, img :"image-13.jfif",price:10000/-,RATING  }]
+//    //  foreach statement
+//    data.forEach(function myFunction( items){
+//     console.log(items.age);
+
+//    })
+
+
+  // Date
+
+//   // present date
+ const date=new Date();
+//  console.log(date);
+
+//   // previous date
+//  console.log(new Date("2021-4-22"));
+//  console.log( new Date("2021-4-22"))
+
+// //  without strings
+//  console.log( new Date(2021-4-22));
+//  console.log( new Date(2021-4-22))
+ 
+// // //  full date
+//   console.log(new Date(2012,12,12,12,12,12,0)); 
+// console.log(new Date(2012,12,12,12,12,12,0))
+
+
+
+// // get method
+
+//  // get year
+//  const year=date.getFullYear();
+//  console.log(year);
+
+// //  get month
+//  const month=date.getMonth();
+//  console.log(month);
+
+// //  get month
+// const months=["January","February","March","April","May","June",,"July","August","September","October","November","December"];
+//  const month1=months[date.getMonth()];
+//  console.log(month1);
+
+// //  get date
+//  const d=date.getDate();
+//  console.log(d);
+
+// //  get day
+//  const day=date.getDay();
+//  console.log(day);
+
+// //  get day
+//  const weeks=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+//  const week=weeks[date.getDay()];
+//  console.log(week);
+
+// //  get hours
+//  const hours=date.getHours();
+//  console.log(hours);
+
+// //  get minutes
+// const minutes=date.getMinutes();
+//  console.log(minutes);
+
+// //  get seconds
+// const seconds=date.getSeconds();
+//  console.log(seconds);
+
+// //  get milliseconds
+// const milliseconds=date.getMilliseconds();
+//  console.log(milliseconds);
+ 
+
+  // set method 
+ // get year
+//  const year=date.setFullYear(2021);
+//  console.log(date);
+
+// //  set month
+//  const month=date.setMonth(5);
+//  console.log(date);
+//  const month1=date.setMonth(19);
+//  console.log(date);
+
+// //  set date
+//  const da =date.setDate(29);
+//  console.log(date);
+//  const da1 =date.setDate(40);
+//  console.log(date);
+
+// // //  set day
+//  const day=date.setDay("wednesday");
+//  console.log(date);
+
+// //  set day
+//  const weeks=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+//  const week=weeks[date.setDay(9)];
+//  console.log(date);
+
+// //  set hours
+//  const hours=date.setHours(27);
+//  console.log(date);
+
+// //  set minutes
+// const minutes=date.setMinutes(89);
+//  console.log(date);
+
+// //  set seconds
+// const seconds=date.setSeconds(234);
+//  console.log(date);
+
+// //  set milliseconds
+// const milliseconds=date.setMilliseconds(4655465);
+//  console.log(date);
+
+
+//  math metods
+
+// round method
+// console.log(Math.round(7.9));
+// console.log(Math.round(7.5));
+// console.log(Math.round(7.2));
+// console.log(Math.round(-7.9));
+// console.log(Math.round(-7.5));
+// console.log(Math.round(-7.2));
+
+// ceil method
+// console.log(Math.ceil(7.9));
+// console.log(Math.ceil(7.5));
+// console.log(Math.ceil(7.2));
+// console.log(Math.ceil(-7.9));
+// console.log(Math.ceil(-7.5));
+// console.log(Math.ceil(-7.2));
+// console.log(Math.ceil(-7.0));
+
+// // floor method
+// console.log(Math.floor(7.9));
+// console.log(Math.floor(7.5));
+// console.log(Math.floor(7.2));
+// console.log(Math.floor(-7.9));
+// console.log(Math.floor(-7.5));
+// console.log(Math.floor(-7.2));
+
+// // turnc method
+// console.log(Math.trunc(7.9));
+// console.log(Math.trunc(-8.2));
+
+// // // sign method
+// console.log(Math.sign(7.9));
+// console.log(Math.sign(-7.2));
+
+// // sqrt method
+// console.log(Math.sqrt(625));
+// console.log(Math.sqrt(-625));
+
+// // absolute method
+// console.log(Math.abs(7.2));
+// console.log(Math.abs(-7.9));
+
+// // power method
+// console.log(Math.pow(2,3));
+// console.log(Math.pow(-2,-5));
+// console.log(Math.pow(-2,5));
+// console.log(Math.pow(2,-5));
+
+// random method
+// console.log(Math.random());
+// console.log(Math.random()*10);
+// console.log(Math.floor(Math.random()));
+// console.log(Math.floor(Math.random()*10));
+
+// max and min method
+// const array=[5426316,545462536,767368,92178479,7364892789,5737439,3984];
+// console.log(Math.max.apply(null,array));
+// console.log(Math.min.apply(null,array));
+
+
+// Set interval
+// setInterval(function myFunction(){
+//   console.log("Hello");
+// },1000);
+
+// set timeout
+// setTimeout(function myFunction(){
+//   console.log("Hello");
+// },1000);
+
+// clear interval
+// const interval=setInterval(function myFunction(){
+//   console.log("Hello");
+// },3000);
+// setTimeout(function myFunction(){
+//   clearInterval(interval);
+//    },12000);
+
+// const li=document.createElement(".li");
+// const li1=document.createElement(".li1");
+// const li2=document.createElement(".li2");
+// const li4=document.createElement(".li4");
+// const li5=document.createElement(".li5");
+// li.innerText="Home";
+// li1.innerText="Aboutus";
+// li2.innerText="Services";
+// li3.innerText="Link";
+// li4.innerText="Dropdown";
+// li5.innerText="Action";
+
+
+// div.appendChild(li);
+// li.appendChild(li);
+// todo.appendChild(div);
+
+// conditional statements
+// if else statement
+const x=50;
+// if(x<50){
+//   console.log("Hey");
+// }
+//  else if (x>70){
+//   console.log("Hello");
+// } else{  
+//   console.log("Hii");
+// }
+
+// // if else with time
+
+// const time=new Date().getHours();
+// let times;
+// if(time<11){
+//   times="Good morning";
+// }else if(time>12){
+//   times="Good afternoon";
+// }else{
+//   times="good evening";
+// }
+// console.log(times);
+
+// // if statement
+
+// const y=50;
+// if(y<50) {
+//   console.log("Hey");
+// }
+//  else{
+//   console.log("Hello");
+// }
+
+// // switch statement
+
+// const day=new Date().getDay();
+// let days;
+// switch(day){
+//   case 0:
+//   days="Sunday"
+//   break
+//   case 1:
+//   days= "Monday"
+//   break
+//   case 2:
+//   days="Tuesday"
+//   break
+//   case 3:
+//   days="Wednesday"
+//   break
+//   case 4:
+//   days="Thursday"
+//   break
+//   case 5:
+//   days="Friday"
+//   break
+//   case 6:
+//   days="Saturday"
+//   break
+//  default:
+// days="Hii"
+// }
+// console.log(days);
+
+// // loops
+// for loop
+// for(let i=0;i<10;i++){
+//   console.log(i);
+// }
+
+// // while loop
+// let i=0;
+// while(i<10){
+//   console.log(i);
+//   i++;
+// }
+// // };
+
+// // Do while loop
+// let z=1;
+// do{
+//   console.log(z);
+//   z++;
+// }
+// while(z<5);                       
+
+// Dom intro
+const todo=document.querySelector(".todo");
+const div=document.createElement(".div");
+todo.appendChild(div);
+
